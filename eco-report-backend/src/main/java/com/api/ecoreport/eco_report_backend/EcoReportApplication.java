8c0aa5a989ed100a24200dc7feb45062cda0f816
Sabrina <sabrinaslimap@gmail.com>
2024-07-29 12:08:20 -0300

    feat: adiciona estrutura inicial do projeto Spring Boot

---
+++ eco-report-backend/src/main/java/com/api/ecoreport/eco_report_backend/EcoReportApplication.java
@@ -0,0 +1,13 @@
+package com.api.ecoreport.eco_report_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoReportApplication.class, args);
+	}
+
+}
---
+++ eco-report-backend/src/test/java/com/api/ecoreport/eco_report_backend/EcoReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ecoreport.eco_report_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
