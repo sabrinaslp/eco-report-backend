a03cfc0a7047f81f9c09527914e1a48fd29ae0d5
Sabrina <sabrinaslimap@gmail.com>
2024-07-29 18:03:50 -0300

    feat: adiciona entidade Notification

---
+++ eco-report-backend/src/main/java/com/api/ecoreport/model/Notification.java
@@ -0,0 +1,34 @@
+package com.api.ecoreport.model;
+
+import com.api.ecoreport.model.enums.NotificationType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "notifications")
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType type;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    private LocalDateTime timestamp;
+
+    private boolean read;
+
+}
---
+++ eco-report-backend/src/main/java/com/api/ecoreport/model/enums/NotificationType.java
@@ -0,0 +1,6 @@
+package com.api.ecoreport.model.enums;
+
+public enum NotificationType {
+    REPORT_STATUS_UPDATE,
+    GENERAL
+}
